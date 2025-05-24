d56de024d3a2b4cd7f12d3b0349f3ad47404ea9d
MathildeSunwell <Mathildesunwell@gmail.com>
2025-05-24 15:50:30 +0200

    initial commit

---
+++ gaming-directory/src/main/java/com/example/gaming_directory/GamingDirectoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.gaming_directory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamingDirectoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamingDirectoryApplication.class, args);
+	}
+
+}
